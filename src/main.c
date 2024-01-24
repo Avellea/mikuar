d4e7505577e3f405ee7227178808324b625e411f
Avellea <dynamicdonkey0@gmail.com>
2024-01-24 12:55:26 -0600

    Fix mysterious bug preventing usage

--- src/main.c
+++ src/main.c
@@ -1,31 +1,58 @@
 #include <psp2/kernel/clib.h>
 #include <psp2/kernel/modulemgr.h>
-#include <psp2/rtc.h> 
+#include <psp2/rtc.h>
+#include <psp2/apputil.h>
 
 #include <taihen.h>
 
 #define printf sceClibPrintf
+#define SCE_SYSTEM_PARAM_ID_TIME_ZONE 6
 
 
 static tai_hook_ref_t app_start_ref;
 
 SceUID gHook = -1;
 
+static int saved_now = 0;
+static int tzoffset = 0;
+//static int desiredTime = 1374091200; // Wed Jul 17 2013 20:00:00 GMT+0000
+static int desiredTime = 1374003000; // Wed Jul 17 2013 20:00:00 GMT+0000
 
 int fakeSystemDateTime(int *t) {
-  static int offset = 0;
-  int desiredTime = 1374021000; // 13 July 2013, 19:30
   int ret = TAI_CONTINUE(int, app_start_ref, t);
-  if (!offset) {
-    offset = ret - desiredTime;
+  if (saved_now == 0) {
+    saved_now = ret;
+    printf("now: %d\n", ret);
+    printf("set saved_now\n");
+    printf("returning: %d\n", desiredTime + (ret - saved_now));
   }
   if (t != NULL)
-    *t = ret - offset;
-  return ret - offset;
+    *t = desiredTime + (ret - saved_now) - tzoffset;
+  return desiredTime + (ret - saved_now) - tzoffset;
 }
 
 void _start() __attribute__ ((weak, alias ("module_start")));
 int module_start(SceSize argc, const void *args) {
+
+  SceAppUtilInitParam init;
+  SceAppUtilBootParam boot;
+  sceClibMemset(&init, 0, sizeof(SceAppUtilInitParam));
+  sceClibMemset(&boot, 0, sizeof(SceAppUtilBootParam));
+
+  SceInt ret = 0;
+  ret = sceAppUtilInit(&init, &boot);
+  printf("ret: 0x%08x\n", ret);
+
+  SceInt tzoffset_minutes = 0;
+
+  sceAppUtilSystemParamGetInt(SCE_SYSTEM_PARAM_ID_TIME_ZONE, &tzoffset_minutes);
+  printf("tzoffset_minutes: %d\n", tzoffset_minutes);
+
+  tzoffset = tzoffset_minutes * 60;
+
+  ret = sceAppUtilShutdown();
+  printf("ret: 0x%08x\n", ret);
+
   gHook = taiHookFunctionExport(
     &app_start_ref,
     "SceLibc",
